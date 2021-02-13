f3b8de252f5195a198d989892ae492ed74c432a0
josue <tshadez@hotmail.com>
2021-02-13 00:40:59 -0500

    nuevas cosas | y cambios | 13/02/2021

--- views/template/scripts_error.php
+++
@@ -1,9 +0,0 @@
-<script src="<?php echo server_url; ?>assets/vendor/jquery/jquery-3.3.1.min.js"></script>
-<script src="<?php echo server_url; ?>assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
-<script src="<?php echo server_url; ?>assets/vendor/slimscroll/jquery.slimscroll.js"></script>
-<script src="<?php echo server_url; ?>assets/libs/js/main-js.js"></script>
-<script src="<?php echo server_url; ?>assets/js/functions_ad.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-</body>
-</html>
