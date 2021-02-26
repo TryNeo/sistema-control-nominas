9cda9959c53e2252d499e6d9236edab374db5d61
Josu3 lopez <ts.josu3@gmail.com>
2021-02-26 18:27:16 -0500

    .

---
+++ models/EmpleadosModel.php
@@ -0,0 +1,11 @@
+<?php
+    require_once("./libraries/core/mysql.php");
+    class EmpleadosModel extends Mysql{
+        public function __construct(){
+            parent::__construct();
+        }
+
+        
+    }
+
+?>
